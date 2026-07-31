d93bb9df48f93a2293b6c69540bc6113340b483a
Dhanavarshini Sakthivel <sdhanavarshinisdhanavarshini@gmail.com>
2026-07-31 15:37:50 +0530

    Time: 3 ms (12.23%) | Memory: 43.1 MB (10.72%) - LeetSync

---
+++ 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String toHex(int num) {
+        if(num == 0) return "0";
+        
+        char[] hex = {'0','1','2','3','4','5','6','7','8','9',
+                      'a','b','c','d','e','f'};
+        
+        String result = "";
+        
+        while(num != 0){
+            result = hex[num & 15] + result;
+            num >>>= 4;
+        }
+        
+        return result;
+    }
+}

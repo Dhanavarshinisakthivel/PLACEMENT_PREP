24821fbfe5066feacc87e7ffc859130598681ed5
Dhanavarshinisakthivel <sdhanavarshinisdhanavarshini@gmail.com>
2026-08-30 14:41:54 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (51.51%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,15 +1,14 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        s=s.trim();
-        int count=0;
-        for(int i=s.length()-1;i>=0;i--)
-        {
-            if(s.charAt(i)==' ')
-            {
-                break;
-            }
+        s = s.trim(); // remove spaces at both ends
+
+        int count = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == ' ') break;
             count++;
         }
+
         return count;
     }
 }
